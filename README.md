e5a110751fb2db4489e2fe5460efe24fbbdce31a
Ronaldo Adriano Marques da Silva <ronaldo.rendadigital@gmail.com>
2023-02-16 22:50:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+FAZENDO DOWNLOAD DO INSTALADOR & INICIANDO A PRIMEIRA INSTALAÇÃO (USAR SOMENTE PARA PRIMEIRA INSTALAÇÃO):
+
+```bash
+sudo apt install -y git && git clone https://github.com/firezap/fzsi.git && sudo chmod -R 777 fzsi && cd fzsi && sudo ./install_primaria
+```
+
+ACESSANDO DIRETORIO DO INSTALADOR & INICIANDO INSTALAÇÕES ADICIONAIS (USAR ESTE COMANDO PARA SEGUNDA OU MAIS INSTALAÇÃO:
+```bash
+cd ./fzsi && sudo ./install_instancia
+```
+
---
+++ front/build/precache-manifest.9ca409f117066a21e1f3b29821981bcb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b75cef1c7d98837cb479728ebbc0ef6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e1d16390ba4df27aca1",
+    "url": "/static/css/2.fa426b8c.chunk.css"
+  },
+  {
+    "revision": "2e1d16390ba4df27aca1",
+    "url": "/static/js/2.4c9896ec.chunk.js"
+  },
+  {
+    "revision": "e32ccaf4a0109157023fabb2ad785ad7",
+    "url": "/static/js/2.4c9896ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d2ab0a3cf82896172a",
+    "url": "/static/js/3.525a8b8c.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.525a8b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "000a85af1b88b2995e39",
+    "url": "/static/js/main.d9d46913.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.d9d46913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b60a8559a710d29e726",
+    "url": "/static/js/runtime-main.154bc6db.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "84b97029ce122e708a75677533a89507",
+    "url": "/static/media/zapsimples.84b97029.png"
+  }
+]);
